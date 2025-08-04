9ef3cb62a297a2fdbc25918a7397bf8cd0d228a8
Karen Collins <fisheskc@googlemail.com>
2025-08-04 19:32:30 +0100

    this is my second commit

--- middleware.ts
+++ middleware.ts
@@ -27,9 +27,13 @@ export default clerkMiddleware((auth, req) => {
 
   // These are going to be public routes
   // We will actually look for the routes that are not in our createRouteMatcher
-  if (!isPublicRoute(req)) {
-    auth().protect(); // Protect the route if it matches the defined criteria
+  // if (!isPublicRoute(req)) {
+  //   auth().protect(); // Protect the route if it matches the defined criteria
+  // }
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL('/', req.url));
   }
+   if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
