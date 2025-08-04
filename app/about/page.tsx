4bb0a285f864edc0ee1a0097d2384383847cb4f7
Karen Collins <fisheskc@googlemail.com>
2025-08-04 22:29:48 +0100

    About page all single quotes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,9 @@
 function AboutPage() {
   return (
     <section>
-      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+      <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
         We love
-        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">
+        <span className='bg-primary py-2 px-4 rounded-lg tracking-widest text-white'>
           store
         </span>
       </h1>
