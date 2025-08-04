142a88854ce7e2ae65119ec7cf9c62fb5017b061
Karen Collins <fisheskc@googlemail.com>
2025-08-04 22:21:44 +0100

    this is my about page with a single quote

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ function AboutPage() {
           store
         </span>
       </h1>
-      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground">
+      <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero hic
         distinctio ducimus temporibus nobis autem laboriosam repellat, magni
         fugiat minima excepturi neque, tenetur possimus nihil atque! Culpa nulla
