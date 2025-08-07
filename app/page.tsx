1630ea6cdbe2994bf56cbd096d70a719800783eb
Karen Collins <fisheskc@googlemail.com>
2025-08-07 16:25:29 +0100

    Updated the about & home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import LoadingContainer from '@/components/global/LoadingContainer';
 import FeaturedProducts from '@/components/home/FeaturedProducts';
-import Hero from '@/components/home/Hero';
-import { Suspense } from 'react';
+import Hero from "@/components/home/Hero";
+import  { Suspense } from 'react'
+
+// fallback will be our loading container
 function HomePage() {
   return (
     <>
-      <Hero />
-
-      <Suspense fallback={<LoadingContainer />}>
+     <Hero />
+     <Suspense fallback = {<LoadingContainer />}>
         <FeaturedProducts />
-      </Suspense>
+     </Suspense>
     </>
-  );
+  )
 }
+
 export default HomePage;
